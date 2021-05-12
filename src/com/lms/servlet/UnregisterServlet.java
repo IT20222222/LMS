aa877979eedd714c2287749d42a935adfe0cb581
SKShehan <shehansilva2013@gmail.com>
2021-05-12 12:12:08 +0530

    Added validation before deletion of user profile

--- src/com/lms/servlet/UnregisterServlet.java
+++ src/com/lms/servlet/UnregisterServlet.java
@@ -1,6 +1,7 @@
 package com.lms.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -18,25 +19,45 @@ import com.lms.util.UserDBUtil;
 public class UnregisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	//Deleting the user information by passing the ID of the logged in user to the Database Util after checking for correct credentials.
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		PrintWriter out = response.getWriter();
+		
 		HttpSession session = request.getSession();
-		
 		User user = (User) session.getAttribute("loggedUser");
-		
 		int id = user.getId();
 		
-		boolean isTrue = UserDBUtil.DeleteUser(id);
+		//Checking whether the credentials given are correct to process deletion.
 		
-		if(isTrue == true) {
-			RequestDispatcher dis1 = request.getRequestDispatcher("Success.jsp");
-			dis1.forward(request, response);
+		String username = request.getParameter("username");
+		String password = request.getParameter("Password");
+		boolean credentials = UserDBUtil.validate(username, password);
+		
+		if(credentials == true) {  //If credentials are correct , the deletion will take place.
+			
+			boolean isTrue = UserDBUtil.DeleteUser(id);
+			
+			if(isTrue == true) {
+				RequestDispatcher dis1 = request.getRequestDispatcher("Success.jsp");
+				dis1.forward(request, response);
+			}
+			else {
+				RequestDispatcher dis2 = request.getRequestDispatcher("Unsuccess.jsp");
+				dis2.forward(request, response);
+			}
 		}
+		
+		//If credentials are wrong display a message.
 		else {
-			RequestDispatcher dis2 = request.getRequestDispatcher("Unsuccess.jsp");
-			dis2.forward(request, response);
+			out.print("<script type = 'text/javascript'>");
+			out.print("alert('Your Password or Username is incorrect.');");
+			out.print("location = 'Unregister_Profile.jsp'");
+			out.print("</script>");
 		}
+		
+		
 	}
 
 }
